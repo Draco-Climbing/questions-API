ae87349d0587a67a6c77cb9337c0dd4b972dfdf6
Matthew Olsen <mbolsen@gmail.com>
2021-12-01 14:36:36 -0700

    Done: fixed errors for EC2 and docker

--- connect.sh
+++ connect.sh
@@ -1,11 +1,12 @@
 case $1 in
 
 mongo)
-  scp -i hackreact.pem ./init.sh ec2-user@ec2-18-119-161-215.us-east-2.compute.amazonaws.com:~/init.sh
-  ssh -i hackreact.pem ec2-user@ec2-18-119-161-215.us-east-2.compute.amazonaws.com
-
+  scp -i "sdc-key-pair.pem" ./init.sh ec2-user@ec2-18-118-134-237.us-east-2.compute.amazonaws.com:~/init.sh
+  scp -i "sdc-key-pair.pem" ./data/*.csv ec2-user@ec2-18-118-134-237.us-east-2.compute.amazonaws.com:~/questions-API/data/
+  ssh -i sdc-key-pair.pem ec2-user@ec2-18-118-134-237.us-east-2.compute.amazonaws.com
+  ;;
 node)
-  scp -i hackreact.pem ./init.sh ec2-user@ec2-52-15-209-146.us-east-2.compute.amazonaws.com:~/init.sh
-  ssh -i hackreact.pem ec2-user@ec2-52-15-209-146.us-east-2.compute.amazonaws.com
+  scp -i "sdc-key-pair.pem" ./init.sh ec2-user@ec2-3-19-219-97.us-east-2.compute.amazonaws.com:~/init.sh
+  ssh -i "sdc-key-pair.pem" ec2-user@ec2-3-19-219-97.us-east-2.compute.amazonaws.com
   ;;
 esac
--- init.sh
+++ init.sh
@@ -1,12 +1,14 @@
-sudo yum update -yum
-sudo yum install git -yum
-sudo yum install docker -yum
+sudo yum update 
+sudo yum install git
+sudo amazon-linux-extras install docker
+# sudo yum install docker
 
 git clone https://github.com/Draco-Climbing/questions-API.git
 
 # Install docker compose
 sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bn/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+# sudo mv /usr/local/bin/docker-compose /usr/local/bin/docker-compose
 docker-compose version
 
 #start docker
