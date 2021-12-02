5716a3582e7a7712a40445734d6f983da90f34ae
Matthew Olsen <mbolsen@gmail.com>
2021-12-02 11:29:59 -0700

    fixed merge conflict?

--- database/index.js
+++ database/index.js
@@ -6,7 +6,7 @@ const { answersWithPhotos, questionsWithAnswers } = require('../server/controlle
 //connect to mongo on localhost
 const db = mongoose.createConnection()
 const connectToDB = async function () {
-  await db.openUri('mongodb://sdc-mongo:27017/sdc-questions')
+  await db.openUri('mongodb://:27017/sdc-questions')
 }
 connectToDB()
 // mongoose.connect('mongodb://sdc-mongo:27017/sdc-questions', { family: 4 })
